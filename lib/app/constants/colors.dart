f5a2c01a6cac2eec4ed738bd7a81bd38b884e05c
shadat <shadat.rahman.464@gmail.com>
2023-06-05 22:27:07 +0600

    added poppins font and color

---
+++ lib/app/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color surface = Color(0xffE7DDDA);
+  static const Color main = Color(0xff9A7265);
+  static const Color surface2 = Color(0xffD8E4E8);
+  static const Color main2 = Color(0xff4C7380);
+  static const Color surface3 = Color(0xffE0EBE2);
+  static const Color main3 = Color(0xff659A6E);
+}
